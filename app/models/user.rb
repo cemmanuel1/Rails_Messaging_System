5aa9b27ab66bdf8cf571b3e9c0c08ea8c0dc3a94
Stefanos Ioannou <stefioanec@gmail.com>
2013-12-20 21:08:03 +0000

    Fixing attr_accessible for devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
+  
   has_many :messages
 end
